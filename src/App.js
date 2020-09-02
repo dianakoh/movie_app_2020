cb83359cd6a58d452c926ba9a4283327307e6cb2
dianakoh <dianakoh94@gmail.com>
2020-09-03 02:56:13 +0900

    add css style to movie app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [fetchedMovies, setFetchedMovies] = useState([]);
+
+  const getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    setFetchedMovies(movies);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  // useEffect(() => {
+  //   fetchedMovies.length !== 0 && console.log(fetchedMovies);
+  // }, [fetchedMovies]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="container">
+      {isLoading ? (
+        <div className="loader">
+          <span className="loader__text">Loading...</span>
+        </div>
+      ) : (
+        <div className="movies">
+          {fetchedMovies.map((movie) => (
+            <Movie
+              key={movie.id}
+              id={movie.id}
+              year={movie.year}
+              title={movie.title}
+              summary={movie.summary}
+              poster={movie.medium_cover_image}
+              genres={movie.genres}
+              back={movie.background_image_original}
+            />
+          ))}
+        </div>
+      )}
+    </section>
   );
 }
 
